6ca764b87697525a5285cf4fa42e83461d02c22d
Eric Da Cruz <ericdacruz.dev@gmail.com>
2023-12-16 20:56:14 -0300

    feat: Add CRUD endpoints for courses

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,8 +2,24 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-   return res.status(200).json({ message: "Hello World Ignite" });
+app.get("/courses", (req, res) => {
+   return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (req, res) => {
+   return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (req, res) => {
+   return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+   return res.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+   return res.json(["Curso 6", "Curso 7", "Curso 4"]);
 });
 
 app.listen(3333, () => console.log("Server running http://localhost:3333"));
