61705db4eb4fe61361888c0fc5c89a708e338f15
Eric Da Cruz <ericdacruz.dev@gmail.com>
2023-12-18 22:14:13 -0300

    feat: Add uuid package and implement account creation endpoint

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -1,8 +1,26 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 
 app.use(express.json());
 
+const customers = [];
+
+app.post("/account", (req, res) => {
+   const { cpf, name } = req.body;
+   const id = uuidv4();
+
+   customers.push({
+      cpf,
+      name,
+      id,
+      statement: [],
+   });
+
+   console.log(customers);
+
+   return res.status(201).send();
+});
 
 app.listen(3333, () => console.log("Server running http://localhost:3333"));
