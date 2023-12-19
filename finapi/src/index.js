9b7cf842d6e44fd02f93a658bf0d1831a058e8da
Eric Da Cruz <ericdacruz.dev@gmail.com>
2023-12-19 15:52:09 -0300

    feat: Add withdraw functionality to API

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -21,6 +21,18 @@ function verifyIfExistAccountCPF(req, res, next) {
    return next();
 }
 
+function getBalance(statement) {
+   const balance = statement.reduce((acc, operation) => {
+      if (operation.type === "credit") {
+         return acc + operation.amount;
+      } else {
+         return acc - operation.amount;
+      }
+   }, 0);
+
+   return balance;
+}
+
 app.post("/account", (req, res) => {
    const { cpf, name } = req.body;
 
@@ -58,6 +70,27 @@ app.post("/deposit", verifyIfExistAccountCPF, (req, res) => {
       amount,
       created_at: new Date(),
       type: "credit",
+   };
+
+   customer.statement.push(statementOperation);
+
+   res.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistAccountCPF, (req, res) => {
+   const { customer } = req;
+   const { amount } = req.body;
+
+   const balance = getBalance(customer.statement);
+
+   if (balance < amount) {
+      return res.status(400).json({ error: "Insufficient funds!" });
+   }
+
+   const statementOperation = {
+      amount,
+      created_at: new Date(),
+      type: "debit",
    };
 
    customer.statement.push(statementOperation);
