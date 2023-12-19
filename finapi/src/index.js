95f175e5adf02d63badb7b226c88d7fdf9ef394b
Eric Da Cruz <ericdacruz.dev@gmail.com>
2023-12-19 15:31:03 -0300

    feat: Add middleware to verify if account exists by CPF

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -7,6 +7,20 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistAccountCPF(req, res, next) {
+   const { cpf } = req.headers;
+
+   const customer = customers.find((customer) => customer.cpf === cpf);
+
+   if (!customer) {
+      return res.status(400).json({ error: "Customer not found!" });
+   }
+
+   req.customer = customer;
+
+   return next();
+}
+
 app.post("/account", (req, res) => {
    const { cpf, name } = req.body;
 
@@ -28,15 +42,10 @@ app.post("/account", (req, res) => {
    return res.status(201).send();
 });
 
-app.get("/statement", (req, res) => {
-   const { cpf } = req.headers;
+//app.use(verifyIfExistAccountCPF); -> Everything below this line will use this middleware
 
-   const customer = customers.find((customer) => customer.cpf === cpf);
-
-   if (!customer) {
-      return res.status(400).json({ error: "Customer not found!" });
-   }
-
+app.get("/statement", verifyIfExistAccountCPF, (req, res) => {
+   const { customer } = req;
    return res.status(200).json(customer.statement);
 });
 
