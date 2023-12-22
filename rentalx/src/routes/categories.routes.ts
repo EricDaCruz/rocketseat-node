20142ef5ef6deac60c340d191389332de0249026
Eric Da Cruz <ericdacruz.dev@gmail.com>
2023-12-22 15:35:52 -0300

    feat: Add categories route and enable debugging in dev script

---
+++ rentalx/src/routes/categories.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+
+const categoriesRoutes = Router();
+
+interface ICategory {
+    name: string;
+    description: string;
+}
+
+const categories: ICategory[] = [];
+
+categoriesRoutes.post("/categories", (request, response) => {
+    const { name, description }: ICategory = request.body;
+
+    categories.push({ name, description });
+
+    return response.status(201).send();
+});
+
+export { categoriesRoutes };
--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
-app.get("/", (request, response) => {
-    response.json({ message: "Hello World !!" });
-});
+app.use(express.json());
+
+app.use(categoriesRoutes);
 
 app.listen(3333, () => {
     console.log("Server is running on port http://localhost:3333");
