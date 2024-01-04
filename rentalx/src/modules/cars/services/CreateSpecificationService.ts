e25edf976ef38a610be3e0290085d7cdc466eab0
Eric Da Cruz <ericdacruz.dev@gmail.com>
2024-01-04 16:21:48 -0300

    feat: Add specifications repository and service

--- rentalx/src/modules/cars/services/CreateSpecificationService.ts
+++ rentalx/src/modules/cars/services/CreateSpecificationService.ts
@@ -1,5 +1,22 @@
+import { type ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
 class CreateSpecificationService {
-    execute({ name, description }): void {}
+    constructor(private specificationRepository: ISpecificationsRepository) {}
+
+    execute({ name, description }: IRequest): void {
+        const specificationAlreadyExists =
+            this.specificationRepository.findByName(name);
+
+        if (specificationAlreadyExists) {
+            throw new Error("Specification already exists!");
+        }
+        this.specificationRepository.create({ name, description });
+    }
 }
 
 export { CreateSpecificationService };
